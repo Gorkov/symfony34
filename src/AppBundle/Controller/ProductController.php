f39ba0735b6e68a4ccdd6033942fdff04a70f4c6
gorkov <gorkov.as@yandex.ru>
2019-03-08 14:48:18 +0400

    added products page

--- src/AppBundle/Controller/ProductController.php
+++ src/AppBundle/Controller/ProductController.php
@@ -1,10 +1,34 @@
 <?php
 
-declare(strict_types=1);
-
 namespace AppBundle\Controller;
 
-class ProductController
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ProductController extends Controller
 {
+
+    /**
+     * @Route("/products", name="productList")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        $products = [];
+
+        for ($i = 1; $i <= 20; $i++) {
+            $products[] = rand();
+        }
+
+        return $this->render(
+            '@App/product/index.html.twig',
+            [
+                'products' => $products,
+            ]
+        );
+    }
 
 }
