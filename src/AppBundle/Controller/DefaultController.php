021a5a3cb58b77ef9c657e590096faccc1606c68
gorkov <gorkov.as@yandex.ru>
2019-03-08 11:20:03 +0400

    init

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     * @param Request $request
+     *
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render(
+            'default/index.html.twig',
+            ['name' => 'Anatolii','surname'=>'Gorkov']
+        );
+    }
+
+    /**
+     * @Route("/feedback", name="feedback")
+     */
+    public function feedbackAction()
+    {
+        return $this->render(
+            'default/feedback.html.twig',
+            ['name' => 'Anatolii']
+        );
+    }
+}
